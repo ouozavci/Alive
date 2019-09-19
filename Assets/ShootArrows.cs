6600d3b9aca7ad7406df76efd22842e1989d1e80
Oğuzhan Özavcı <ouozavci@gmail.com>
2019-09-19 23:22:40 +0300

    first commit

---
+++ Assets/ShootArrows.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootArrows : MonoBehaviour
+{
+    public GameObject arrow;
+    public GameObject bow;
+    public float arrowSpeed;
+    public float despawnTime = 3.0f;
+    public bool shootable = true;
+    public float waitBeforeNextShot = 0.25f;
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && shootable)
+        {
+            setTargetPosition();
+            shoot();
+            shootable = false;
+            StartCoroutine(ShootingYield());
+        }
+    }
+
+    void setTargetPosition(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 1000)){
+            Vector3 target = hit.point;
+            this.transform.LookAt(target);
+        }
+    }
+
+    IEnumerator ShootingYield()
+    {
+        yield return new WaitForSeconds(waitBeforeNextShot);
+        shootable = true;
+    }
+
+    void shoot()
+    {
+        var bullet = Instantiate(arrow, bow.transform.position, bow.transform.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = -1 * bullet.transform.up * arrowSpeed ;
+
+        Destroy(bullet, despawnTime);
+    }
+}
---
+++ Assets/Zombi.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombi : MonoBehaviour
+{
+    //This moves the player with a slope of 1 or undefined. Very limited
+
+    public float speed = 2.0f;
+    public float attackDistance = 5.0f;
+    public GameObject player; //Player object must have the tag 'Player';
+    private CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void FixedUpdate()
+    {
+        
+        if (Vector3.Distance(transform.position, player.transform.position) > attackDistance)
+        {
+            moveDirection = calculateDirection(transform.position, player.transform.position);
+            controller.Move(moveDirection * Time.deltaTime * speed);
+        
+        }else{
+            attack();
+        }
+    }
+
+    private void attack(){
+        Debug.Log("Attacking player");
+    }
+
+    private Vector3 calculateDirection(Vector3 startPoint, Vector3 destination)
+    {
+        float startX = startPoint.x;
+        float startZ = startPoint.z;
+        float destinationX = destination.x;
+        float destinationZ = destination.z;
+
+        float xDiff = distanceOfTwoPoints(startX, destinationX);
+        float zDiff = distanceOfTwoPoints(startZ, destinationZ);
+
+
+        if (xDiff == 0 && zDiff == 0) return Vector3.zero;
+        Vector3 direction = new Vector3(xDiff / (xDiff + zDiff), 0, zDiff / (xDiff + zDiff));
+        if (startX > destinationX)
+            direction = Vector3.Scale(direction, new Vector3(-1, 1, 1));
+        if (startZ > destinationZ)
+            direction = Vector3.Scale(direction, new Vector3(1, 1, -1));
+
+        Debug.Log(direction);
+        return direction;
+    }
+
+    private float distanceOfTwoPoints(float point1, float point2)
+    {
+        return Mathf.Abs(point1 - point2);
+    }
+
+}
