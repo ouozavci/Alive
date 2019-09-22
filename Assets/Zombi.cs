79c49f98ee94a28a63da0beb8c5855965de1360c
Oğuzhan Özavcı <ouozavci@gmail.com>
2019-09-22 19:24:45 +0300

    added zombie healt bar - dying of zombies etc.

--- Assets/Zombi.cs
+++ Assets/Zombi.cs
@@ -1,36 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Zombi : MonoBehaviour
 {
     //This moves the player with a slope of 1 or undefined. Very limited
 
+    public float maxHealth;
+    private float health;
+    public GameObject healthBarUI;
+    public Slider healthSlider;
     public float speed = 2.0f;
     public float attackDistance = 5.0f;
     public GameObject player; //Player object must have the tag 'Player';
     private CharacterController controller;
     private Vector3 moveDirection = Vector3.zero;
-
+    private bool isDead = false;
     void Start()
     {
+        health = maxHealth;
+        healthSlider.value = calculateHealth();
         controller = GetComponent<CharacterController>();
     }
     void FixedUpdate()
     {
-        
-        if (Vector3.Distance(transform.position, player.transform.position) > attackDistance)
+        healthSlider.value = this.calculateHealth();
+        if (this.calculateHealth() > 0)
         {
-            moveDirection = calculateDirection(transform.position, player.transform.position);
-            controller.Move(moveDirection * Time.deltaTime * speed);
-        
-        }else{
-            attack();
+            if (Vector3.Distance(transform.position, player.transform.position) > attackDistance)
+            {
+                walkToArcher();
+            }
+            else
+            {
+                attack();
+            }
+        }
+        else if(!isDead)
+        {
+            isDead = true;
+            die();
         }
     }
 
-    private void attack(){
-        Debug.Log("Attacking player");
+    private void walkToArcher()
+    {
+        Debug.Log(controller.isGrounded);
+        moveDirection = calculateDirection(transform.position, player.transform.position);
+        controller.SimpleMove(moveDirection * speed * Time.deltaTime);
+    }
+
+    float calculateHealth()
+    {
+        return health / maxHealth;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("Arrow(Clone)"))
+        {
+            health -= 50;
+            Debug.Log(calculateHealth());
+        }
+
+    }
+
+    private void attack()
+    {
+    }
+
+    private void die()
+    {
+        controller.enabled = false;
+        gameObject.GetComponent<CapsuleCollider>().isTrigger = false;
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.mass = 80;
+        rb.AddForce(calculateDirection(transform.position, player.transform.position) * -1 * 300, ForceMode.Acceleration);
+
+        Destroy(gameObject, 30);
     }
 
     private Vector3 calculateDirection(Vector3 startPoint, Vector3 destination)
@@ -50,8 +97,6 @@ public class Zombi : MonoBehaviour
             direction = Vector3.Scale(direction, new Vector3(-1, 1, 1));
         if (startZ > destinationZ)
             direction = Vector3.Scale(direction, new Vector3(1, 1, -1));
-
-        Debug.Log(direction);
         return direction;
     }
 
