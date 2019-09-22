4fbb94db632d169043e36925d07819816644b871
Oğuzhan Özavcı <ouozavci@gmail.com>
2019-09-22 20:11:39 +0300

    fixed health bar rotations to camera

---
+++ Assets/CanvasCameraRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasCameraRotation : MonoBehaviour
+{
+
+    public Camera mainCamera;
+    
+    private Vector3 startPosition;
+
+    void Start(){
+        startPosition = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.parent.position.x,startPosition.y,transform.parent.position.z);
+        transform.LookAt(transform.position + mainCamera.transform.rotation*Vector3.back);
+    }
+}
