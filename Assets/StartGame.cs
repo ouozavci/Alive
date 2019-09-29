4ea6e4b4d0a2cdd011992d07193cfdbbcc83a1d7
Oğuzhan Özavcı <ouozavci@gmail.com>
2019-09-29 22:18:49 +0300

    start menu created

---
+++ Assets/StartGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public Text loadingText;
+    public Button startButton;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    public void start()
+    {
+        startButton.gameObject.SetActive(false);
+        StartCoroutine(startLoading());
+    }
+
+    IEnumerator startLoading()
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(1);
+        while (!async.isDone)
+        {
+            loadingText.text = "Loading: %" + (int)(((async.progress + 0.1f) * 100f));
+            yield return null;
+        }
+    }
+
+}
